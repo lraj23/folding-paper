8127bf6318c6e4ed7203f0d0758b9a69d31e2811
Lavith Raj <189830163+lraj23@users.noreply.github.com>
2025-11-11 11:29:49 -0800

    allow integration with grid portfolio

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import { getFoldingPaper, saveState } from "./datahandler.js";
 import { foldsInfo } from "./foldsInfo.js";
 const lraj23UserId = "U0947SL6AKB";
 const lraj23BotTestingId = "C09GR27104V";
-const gPortfolioDmId = "C09GR27104V";
+const gPortfolioDmId = "D09SN86RFC1";
 const commands = {};
 const zeroFoldDimensions = [210, 297, 0.1];
 const dimensions = n => {
@@ -19,14 +19,7 @@ const dimensions = n => {
 const dimensionsMin = n => [parseFloat((210 / Math.pow(2, n / 2)).toFixed((210 / Math.pow(2, Math.ceil(n / 2))).toString().split(".").length === 1 ? 0 : (210 / Math.pow(2, Math.ceil(n / 2))).toString().split(".")[1].length)), parseFloat((297 / Math.pow(2, n / 2)).toFixed((297 / Math.pow(2, Math.ceil(n / 2))).toString().split(".").length === 1 ? 0 : (297 / Math.pow(2, Math.ceil(n / 2))).toString().split(".")[1].length)), 0.1 * Math.pow(2, n)];
 const surfaceArea = n => 2 * dimensions(n)[0] * dimensions(n)[1] + 2 * dimensions(n)[0] * dimensions(n)[2] + 2 * dimensions(n)[1] * dimensions(n)[2];
 const landArea = n => dimensions(n)[0] * dimensions(n)[1];
-const [mmInNm, mmInΜm, mmInMm, mmInCm, mmInM, mmInKm] = [
-	0.000001,
-	0.001,
-	1,
-	10,
-	1000,
-	1000000
-];
+const [mmInNm, mmInΜm, mmInMm, mmInCm, mmInM, mmInKm] = [0.000001, 0.001, 1, 10, 1000, 1000000];
 const mmToString = mm => (mm <= 0 ? "0 mm" : (mm < mmInΜm ? (mm / mmInNm) + " nm" : (mm < mmInMm / 10 ? (mm / mmInΜm) + " μm" : (mm < 10 * mmInCm ? mm + " mm" : (mm < mmInM ? (mm / mmInCm) + " cm" : (mm < mmInKm ? (mm / mmInM) + " m" : (mm < 1000 * mmInKm ? (mm / mmInKm) + " km" : (mm / mmInKm).toFixed(0) + " km")))))));
 const mmToString2D = mm => (mm <= 0 ? "0 mm" : (mm < (mmInΜm ** 2) ? (mm / (mmInNm ** 2)) + " nm" : (mm < (mmInMm ** 2) / 10 ? (mm / (mmInΜm ** 2)) + " μm" : (mm < 10 * (mmInCm ** 2) ? mm + " mm" : (mm < (mmInM ** 2) ? (mm / (mmInCm ** 2)) + " cm" : (mm < (mmInKm ** 2) ? (mm / (mmInM ** 2)) + " m" : (mm < 1000 * (mmInKm ** 2) ? (mm / (mmInKm ** 2)) + " km" : (mm / (mmInKm ** 2)).toFixed(0) + " km"))))))) + "²";
 const mmToString3D = mm => (mm <= 0 ? "0 mm" : (mm < (mmInΜm ** 3) ? (mm / (mmInNm ** 3)) + " nm" : (mm < (mmInMm ** 3) / 10 ? (mm / (mmInΜm ** 3)) + " μm" : (mm < 10 * (mmInCm ** 3) ? mm + " mm" : (mm < (mmInM ** 3) ? (mm / (mmInCm ** 3)) + " cm" : (mm < (mmInKm ** 3) ? (mm / (mmInM ** 3)) + " m" : (mm < 1000 * (mmInKm ** 3) ? (mm / (mmInKm ** 3)) + " km" : (mm / (mmInKm ** 3)).toFixed(0) + " km"))))))) + "³";
@@ -95,12 +88,12 @@ const blocks = folds => [
 	{
 		type: "divider"
 	},
-	{ 
+	{
 		type: "section",
 		text: {
 			type: "mrkdwn",
 			text: "The SPACE (yeah, I know) on top of your piece of paper is *" + mmToString2D(landArea(folds)) + "*. On this amount of SPACE, you could fit " + (foldsInfo[folds.toString()] ? foldsInfo[folds.toString()][0] : "something, nobody really knows what (more like I couldn't find anything that made sense, or I haven't researched enough yet, most likely the latter for obvious reasons). What do you know?")
-		},
+		}
 	},
 	{
 		type: "image",
@@ -124,7 +117,29 @@ const blocks = folds => [
 	}
 ];
 
-app.message("", async () => { });
+app.message("", async ({ message: { text, channel, channel_type } }) => {
+	if ((channel_type === "im") && (channel === gPortfolioDmId)) {
+		const info = text.split(";");
+		console.log(info[0], commands[info[0]]);
+		return commands[info[0]]({
+			ack: _ => _,
+			body: {
+				user_id: info[1],
+				channel_id: info[2]
+			},
+			respond: (response) => {
+				if (typeof response === "string") return app.client.chat.postEphemeral({
+					channel: info[2],
+					user: info[1],
+					text: response
+				});
+				if (!response.channel) response.channel = info[2];
+				if (!response.user) response.user = info[1];
+				app.client.chat.postEphemeral(response);
+			}
+		});
+	}
+});
 
 commands.paper = async ({ ack, body: { user_id: user }, respond }) => {
 	await ack();
